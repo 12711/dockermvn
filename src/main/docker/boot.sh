149a54e067ad487ae04be865ea41700b8da3a970
lsm <1076396468@qq.com>
2018-05-01 10:07:11 +0800

    init program

---
+++ src/main/docker/boot.sh
@@ -0,0 +1,1 @@
+java $JAVA_OPTS  -jar /dockermvn.jar
---
+++ src/main/java/com/lsm/dockermvn/DockermvnApplication.java
@@ -0,0 +1,12 @@
+package com.lsm.dockermvn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockermvnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockermvnApplication.class, args);
+	}
+}
